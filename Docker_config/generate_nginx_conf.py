1b7d3dd37eaa358e619f6dcb893a2592cebb88f7
mona233 <xieyumc@gmail.com>
2024-10-21 16:26:44 +0800

    feature：新增管理界面

--- Docker_config/generate_nginx_conf.py
+++ Docker_config/generate_nginx_conf.py
@@ -36,6 +36,20 @@ http {
 
     # 自动生成 location 配置
     for i in range(1, 53):
+        # 为 manage 路径生成配置
+        f.write(f"""
+        # student{i} manage
+        location /student{i}/manage {{
+            proxy_pass http://host.docker.internal:8000/student{i}/manage;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }}
+""")
+        # 为 student 常规路径生成配置
         f.write(f"""
         # student{i}
         location /student{i}/ {{
--- Docker_manage/container_app/urls.py
+++ Docker_manage/container_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_containers, name='list_containers'),
-    path('restart/<str:container_name>/', views.restart_container, name='restart_container'),
-    path('reset/<str:container_name>/', views.reset_container, name='reset_container'),
-    path('<str:container_name>/', views.container_detail, name='container_detail'),
+    path('<str:container_name>/manage/restart', views.restart_container, name='restart_container'),
+    path('<str:container_name>/manage/reset', views.reset_container, name='reset_container'),
+    path('<str:container_name>/manage', views.container_detail, name='container_detail'),
 ]
